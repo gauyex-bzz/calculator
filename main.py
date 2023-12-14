d201f59e125d372f9a17650f550b0d10a772c589
gauyex <gauyex@bzz.ch>
2023-12-14 21:47:05 +0100

    spelling

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-num1 = float(input("Number 1: "))
+num1 = float(input("1st number: "))
 op = input("Operator: ")
 
 while op not in ["+", "-", "*", "/"]:
     print("Operator must be either +, -, * or /")
     op = input("Operator: ")
 
-num2 = float(input("Number 2: "))
+num2 = float(input("2nd number: "))
 
 if op == "+":
     print(num1 + num2)
@@ -16,9 +16,8 @@ elif op == "*":
 elif op == "/":
     while num2 == 0:
         print("Number must not be 0!")
-        num2 = float(input("Number 2: "))
+        num2 = float(input("2nd number: "))
     print(num1 / num2)
-
 
 if __name__ == "__main__":
     True
